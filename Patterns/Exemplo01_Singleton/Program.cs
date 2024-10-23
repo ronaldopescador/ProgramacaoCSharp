de291f54426d2a9eb7d15cf3c04d16b87c489eaf
Ronaldo Pescador <ronaldopescador@gmail.com>
2024-10-23 20:30:08 -0300

    .

--- Patterns/Exemplo01_Singleton/Program.cs
+++ Patterns/Exemplo01_Singleton/Program.cs
@@ -3,10 +3,10 @@
     static void Main(string[] args)
     {
         Logger l1 = Logger.GetInstance();
-        l1.Log("Adicionado ao carrinho");
+        l1.Log("Adicionado ao carrinho.");
 
         Logger l2 = Logger.GetInstance();
-        l1.Log("Efetuado o pagamento");
+        l1.Log("Efetuado o pagamento.");
 
         Console.WriteLine(Object.ReferenceEquals(l1, l2));  
     }
